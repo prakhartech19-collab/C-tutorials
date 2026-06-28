86a4b321b698560ce357dbd6cbfa1f32ce9093ee
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-06-28 12:54:49 +0530

    initial commit

---
+++ helloworld.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+   int L;
+   printf("enter lenth of rectangle :");
+   scanf("%d\n", L);
+
+   int b;
+   printf(" enter breath of rectangle :");
+   scanf("%d\n", b );
+
+   int perimeter = 2*(L +b);
+   printf("%d\n", perimeter);
+return 0;
+
+}
+
