4fa4b8e4875393ebed44ba62528c640e5a22e6a4
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-06-29 13:38:01 +0530

    finding factorial of 5?

--- helloworld.c
+++ helloworld.c
@@ -1,22 +1,19 @@
 #include<stdio.h>
  
- int sum(int n);
+  int fact( int n);
 
  int main() {
-
- printf("sum is :%d", sum(5));
-    return 0;
+     printf("factorial  is :%d", fact(5));
+      return 0;
  }
- // recursive function
-  int sum(int n) {
-    if(n==1){
-        return 1;
-    }
-    int sumNm1= sum(n-1); // sum of 1 to n
-    int sumN = sumNm1 + n;
-    return sumN;
+ 
+  int fact(int n) {
+ if(n == 0) {
+     return 1;
+ }
+  int factNm1 = fact(n-1);
+   int factN = factNm1 * n;
+   return factN;
   }
-
-
   
 
