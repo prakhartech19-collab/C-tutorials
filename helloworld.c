26bc1c563c9e47d2d552304e76ae5634d3bf444e
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-06-29 11:09:27 +0530

    recursive function

--- helloworld.c
+++ helloworld.c
@@ -1,27 +1,22 @@
 #include<stdio.h>
- #include<math.h>
-//area of square
-float squareArea(float side);
-float circleArea(float rad);
-float rectangleArea(float a, float b);
+ 
+void printHW(int count);
 
 int main(){
-   float a=5.0;
-   float b =10.0;
-   
-   printf("area is: %f", rectangleArea(a,b));
+    printHW(5);
     return 0;
 }
+// recursive function
+ void printHW(int count)  {
+    if(count == 0) {
+         return;
+    }
+  
+  printf("Hello world\n");
+  printHW( count -1); 
+ }
 
 
 
-float squareArea(float side) {
-     return side *side;
-}
-float circleArea(float rad){
-    return 3.14 *rad *rad;
-}
+  
 
-float rectangleArea(float a, float b) {
-    return a* b;
-}
