a62448553b6c7b4e9dc59d61c04188864dc46a63
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-06-29 13:22:12 +0530

    sum of first five natural number

--- helloworld.c
+++ helloworld.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
  
-void printHW(int count);
+ int sum(int n);
 
-int main(){
-    printHW(5);
+ int main() {
+
+ printf("sum is :%d", sum(5));
     return 0;
-}
-// recursive function
- void printHW(int count)  {
-    if(count == 0) {
-         return;
-    }
-  
-  printf("Hello world\n");
-  printHW( count -1); 
  }
-
+ // recursive function
+  int sum(int n) {
+    if(n==1){
+        return 1;
+    }
+    int sumNm1= sum(n-1); // sum of 1 to n
+    int sumN = sumNm1 + n;
+    return sumN;
+  }
 
 
   
