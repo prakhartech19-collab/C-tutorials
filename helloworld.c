f2cf27ae2ce763a3e254c252db5680955beb5e81
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-01 17:37:58 +0530

    swaping of values od different variables

--- helloworld.c
+++ helloworld.c
@@ -1,25 +1,19 @@
 #include<stdio.h>
- void square(int n);
-void _square(int *n);
+ void swap( int x, int y);
 
-   int main() {
-    int number = 4;
-    square(number);
-    printf("number = %d\n", number);
-
-    _square(&number);
-    printf("number = %d\n", number);
-     
-    return 0;
+  int main() {
+  int x =3 ;
+  int y = 5;
+  swap(x, y);
+  printf("x = %d& y = %d\n", x, y);
+   return 0;
    }  
-    //call by  value
-   void square(int n) { 
-    n = n * n;
-   }
-    //call by reference 
-    void _square(int *n)  {
-      *n = (*n) * (*n);
-      printf("square = %d\n", *n);
-    }
-  
+   
+   //call by value 
+  void swap(int a , int b) {
+     int t = a;
+     a = b ;
+     b =t;
+     printf("a = %d& b = %d\n", a, b);
+  }
   
