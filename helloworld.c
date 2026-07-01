bec5d2a71f682583f72d3b50a3057653fface811
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-01 20:12:11 +0530

    total price of 3 things  with gst by usein array

--- helloworld.c
+++ helloworld.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
 
-    void doWork( int a , int b , int *sum,  int  *prod , int *avg) ;
-        
+   
  
   int main() {
-     int a = 3 , b = 5;
-     int sum, prod, avg;
-   doWork(a, b, &sum, &prod, &avg);
+      float price[3];
+      printf("enter 3 prices: ");
+      scanf("%f", &price[0]);
+      scanf("%f", &price[1]);   
+      scanf("%f", &price[2]);
 
-   printf("Sum: %d, prod = %d , avg = %d\n", sum , prod , avg);
-   return 0;
-   }  
+      printf("total price 1 : %.2f\n", price[0]+ (0.18 * price[0]));
+      printf("total price 2 : %.2f\n", price[1]+ (0.18 * price[1]));
+      printf("total price 3 : %.2f\n", price[2]+ (0.18 * price[2]));
+    return 0;
+  } 
  
-    void doWork(int a , int b , int *sum, int *prod , int *avg) {
-        *sum = a+ b ;
-        *prod = a*b ;
-        *avg = (a+b) / 2;
-    }
-
+    
     
 
 
