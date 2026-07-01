0d520fe9ffa892c6eb8ad0973169f1edaebab22f
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-01 17:48:01 +0530

    swaping of values through calling by reference

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,26 @@
 #include<stdio.h>
  void swap( int x, int y);
+ void _swap(int*a, int*b);
 
+ 
   int main() {
   int x =3 ;
   int y = 5;
-  swap(x, y);
+  _swap(&x, &y);
   printf("x = %d& y = %d\n", x, y);
    return 0;
    }  
+
+
+//call by reference
+  void _swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b =t;
+
+
+
+  }
    
    //call by value 
   void swap(int a , int b) {
