449240ec2bb0875cac1923262fbab5cc4a217ad9
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-06 09:57:33 +0530

    comparision and difference in pointers arithmatics

--- helloworld.c
+++ helloworld.c
@@ -2,11 +2,14 @@
 
 
 int main() {
-   int age = 22;
-   int *ptr = &age;
- printf("ptr = %u\n", ptr);
-   ptr++;
- printf("ptr = %u\n", ptr);
+    int age =22 ;
+    int _age = 23;
+    int *ptr = &age;
+    int *_ptr = &_age;
+
+    printf("difference =%u\n ", ptr-_ptr);
+    _ptr = &age;
+    printf("comparison =%u\n ", ptr ==_ptr);
     return 0;
 } 
  
