0d82acc84dfd3e9d2fbe68db36030c658780b773
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-06 13:19:22 +0530

    reversing an Array

--- helloworld.c
+++ helloworld.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 
- int countodd(int arr[], int n);
-    
+ void reverse(int arr[], int n);
+  void printArr(int arr[], int n);
+
 int main() {
- int arr[] = {1, 2, 3, 4, 5,6};
- printf("%d",countodd(arr,6));
- 
+ int arr[] = {1,2,3,4,5};
+ reverse(arr, 5);
+ printArr(arr, 5);
  return 0;
 } 
-  int countodd( int arr[], int n) {
-    int count = 0;
-    for(int i = 0; i < n; i++) {
-        if(arr[i] % 2 != 0) {
-            count++;
-        }
-    }
-    return count;
-  } 
+ void printArr(int arr[],int n){
+  for(int i=0; i<n; i++){
+   printf("%d\t",arr[i] );
+  }
+ }
+ void reverse(int arr[], int n) {
+  for(int i = 0; i < n / 2; i++) {
+    int fristval = arr[i];
+    int secondval = arr[n-i-1];
+    arr[i] = secondval;
+    arr[n-i-1] =fristval;
 
+  }
+ }
 
 
   
