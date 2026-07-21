f09ada780986a4e9f7d73292d0c67649f67a8d5a
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-21 16:09:31 +0530

    finding a number which is second max in the bunch of a number

--- helloworld.c
+++ helloworld.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 
     int main() {
-    int n,num,even,odd ;
-    printf("enter a number:");
-    scanf("%d",&n);
-
-    if(n % 2 == 0){
-      printf("number is even");
-    }
-      
-    if(n % 2 != 0){
-      printf("number is odd");
-    }
-      
+    int n,i, max , num , secondmax;
+    printf("enter  number", n);
+    scanf("%d", &n);
     
+    max = -999999;
+     secondmax = -999999;
+    for(int i=0; i<n ; i++){
+      scanf("%d", &num);
+      if( num > max){
+      secondmax =max;
+       max = num ;
+      }
+      else if( num > secondmax  && num < max ){
+
+
+     secondmax  = num ;
+
+      }
+  }
+   printf("second max = %d", secondmax);
+
+
 
 
     return 0;
