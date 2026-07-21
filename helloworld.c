78d8bebf04b68e8ba418f92c86075173c96e59fa
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-21 15:28:09 +0530

    checking the number is odd or even

--- helloworld.c
+++ helloworld.c
@@ -1,25 +1,17 @@
 #include<stdio.h>
 
     int main() {
-    int n,i, num, min;
-     printf("enter n:");
-     scanf("%d", &n);
+    int n,num,even,odd ;
+    printf("enter a number:");
+    scanf("%d",&n);
 
-      printf("enter frist number:");
-     scanf("%d", &min);
-
-      for(int i=1; i< n; i++){
-        printf("enter  number %d:", i);
-        scanf("%d", &num);
-
-         if(num< min){
-        min = num ;
-      }
-      }
-     
-      printf("Min = %d",min);
-
+    if(n % 2 == 0){
+      printf("number is even");
+    }
       
+    if(n % 2 != 0){
+      printf("number is odd");
+    }
       
     
 
