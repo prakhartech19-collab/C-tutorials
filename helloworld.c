93294843244da56943ea7ba64f63f88c6937e706
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-21 15:12:21 +0530

    how to find max num from a bunch of no by useing  combination of for loop an if condition

--- helloworld.c
+++ helloworld.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
     int main() {
-    int n,i, num, max;
+    int n,i, num, min;
      printf("enter n:");
      scanf("%d", &n);
 
       printf("enter frist number:");
-     scanf("%d", &max);
+     scanf("%d", &min);
 
       for(int i=1; i< n; i++){
         printf("enter  number %d:", i);
         scanf("%d", &num);
 
-         if(num> max){
-        max = num ;
+         if(num< min){
+        min = num ;
       }
       }
      
-      printf("Max = %d",max);
+      printf("Min = %d",min);
 
       
       
