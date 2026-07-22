1920d760ba209a3ed52fda638e37cce52986d618
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-22 10:53:23 +0530

    for finding area of rectangle and circle

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
+#include<math.h>
+ // area of rectangle  
 
-    int main() {
-    int a,b,diff;
+int main() {
+    int a,b,Area;
     printf("enter a :");
-    scanf("%d\n", &a);
+    scanf("%d", &a);
 
     printf("enter b: ");
-    scanf("%d\n", &b);
+    scanf("%d", &b);
 
-    diff = a - b ;
-    printf("diff is ;%d\n ", diff);
+    // Area = a* b ;  //for area of rectangle
 
+    // printf("Area is ;%d\n ", Area);
+
+    Area = 3.14 * a * b ;
+    printf("Area is ;%d\n ", Area);
 
     return 0;
   }
