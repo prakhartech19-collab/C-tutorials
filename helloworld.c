1caf19accd1a3e087b2daedd1a444eef9a25d892
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-22 10:38:00 +0530

    for sum or diff of two no we can use it by changing sum or diff

--- helloworld.c
+++ helloworld.c
@@ -1,28 +1,15 @@
 #include<stdio.h>
 
     int main() {
-    int n,i, max , num , secondmax;
-    printf("enter  number", n);
-    scanf("%d", &n);
-    
-    max = -999999;
-     secondmax = -999999;
-    for(int i=0; i<n ; i++){
-      scanf("%d", &num);
-      if( num > max){
-      secondmax =max;
-       max = num ;
-      }
-      else if( num > secondmax  && num < max ){
+    int a,b,diff;
+    printf("enter a :");
+    scanf("%d\n", &a);
 
+    printf("enter b: ");
+    scanf("%d\n", &b);
 
-     secondmax  = num ;
-
-      }
-  }
-   printf("second max = %d", secondmax);
-
-
+    diff = a - b ;
+    printf("diff is ;%d\n ", diff);
 
 
     return 0;
