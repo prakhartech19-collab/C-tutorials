8b28145ab3d741cc807febc4443d9c442247d03b
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-23 16:37:34 +0530

    square of a number by useing function

--- helloworld.c
+++ helloworld.c
@@ -1,46 +1,17 @@
 #include<stdio.h>
  
-int sum(int a , int b);
- int sub( int a , int b);
-float prdct(float a, float b);
-  float Div(float a, float b);
-
+ int square( int num);
 
 int main(){
-   int a,b, choice;
-   printf(" enter two numbers :");
-   scanf( "%d%d", &a,&b);
+ int num;
+ printf("enter a number :");
+ scanf("%d", &num);
 
-   printf("enter your choice(1 to 4):");
-   scanf("%d", &choice);
-
-   switch( choice ){
-    case 1 :printf("sum is :%d\n", sum(a,b));
-          break;
-    case 2 :printf(" diff is :%d\n", sub(a,b));
-         break ;
-    case 3 :printf("product  is :%f\n", prdct(a,b));
-        break;
-    case 4 :if( b !=0){
-    
-    printf(" division is :%f\n", Div(a,b));
-   }else {
-     printf(" zero can not divide any number ");
-   }
+ 
+ printf(" square is :%d",  square(num) );
+   
   return 0;
-  } 
-
-}
- int sum( int a, int b ){
-  return a + b ;
- }
-
- int sub( int a, int b ){
-   return a-b;
- }
- float prdct( float a , float b ){
-  return a * b;
- }
- float Div( float a , float b){
-  return a/b;
- }
+  }
+   int square( int num){
+    return num * num;
+  }
