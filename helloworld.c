c28fd2694371528e49998f4faf75e6768f131024
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-23 15:19:43 +0530

    mini calculator for arithmatic operator

--- helloworld.c
+++ helloworld.c
@@ -1,15 +1,46 @@
 #include<stdio.h>
  
 int sum(int a , int b);
+ int sub( int a , int b);
+float prdct(float a, float b);
+  float Div(float a, float b);
+
 
 int main(){
-   int result;
-   result = sum( 10, 20);
-   printf("sum =%d", result);
- 
+   int a,b, choice;
+   printf(" enter two numbers :");
+   scanf( "%d%d", &a,&b);
+
+   printf("enter your choice(1 to 4):");
+   scanf("%d", &choice);
+
+   switch( choice ){
+    case 1 :printf("sum is :%d\n", sum(a,b));
+          break;
+    case 2 :printf(" diff is :%d\n", sub(a,b));
+         break ;
+    case 3 :printf("product  is :%f\n", prdct(a,b));
+        break;
+    case 4 :if( b !=0){
+    
+    printf(" division is :%f\n", Div(a,b));
+   }else {
+     printf(" zero can not divide any number ");
+   }
   return 0;
+  } 
+
 }
-int sum(int a , int b){
-  return a + b;
-}
- 
+ int sum( int a, int b ){
+  return a + b ;
+ }
+
+ int sub( int a, int b ){
+   return a-b;
+ }
+ float prdct( float a , float b ){
+  return a * b;
+ }
+ float Div( float a , float b){
+  return a/b;
+ }
