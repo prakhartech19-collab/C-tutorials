42e346f24a3f95d65510ccd6d69db9b258ffef80
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-23 12:14:28 +0530

    for reversing the digits of a number

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 
 int main(){
-  int i;
+ int n,rev = 0 , r ;
+ printf("enter numbers :", n);
+ scanf("%d",&n );
+
+ while( n!=0){
+  r= n%10;
+   rev = rev *10 + r;
+   n = n/10;
+ }
+ printf("reversed number is :%d",rev);
+ 
+
  
   
-  for( int i=1; i<=100; i++){
-    if( i % 2 == 0){
-      printf("%d\n", i);
-    }
-  }
   return 0;
 }
