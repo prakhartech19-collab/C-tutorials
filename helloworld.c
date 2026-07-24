c707ed31d94f1e51402aacf5f9b53855d99e4654
Prakhar Tiwari <prakhartecj19@gmail.com>
2026-07-24 17:39:36 +0530

    for reversing the digits of given number

--- helloworld.c
+++ helloworld.c
@@ -1,39 +1,25 @@
 #include<stdio.h>
  
-void prime( );
-
-int n , i;
-int isprime =1;
+int revr(  int n);
+ 
+int n , rev =0 , r;
 
 int main(){
- 
+  printf(" enter a number :");
+  scanf("%d", &n);
 
- printf("enter a number :");
- scanf("%d",&n);
-
- prime();
-
-
+  revr(  n);
 
   return 0;
   }
-   void prime(){
-    if(n<=1){
-        printf(" entered number is not prime ");
-    }else {
-        for(int i =2; i<= n-1; i++){
-        if( n% i ==0){
-            isprime =0;
-            break;
-        }
-        }
-        if(isprime){
-            printf("entered number is prime");
-        } else {
-            printf("entered number is not prime");
-        }
-    }
-   }
-    
-  
-  
+  int   revr( int n){
+    while( n!=0){
+        r = n%10;
+        rev = rev* 10 + r;
+        n = n /10;
+  } 
+  return
+  printf(" reversed number is : %d", rev);
+ 
+  }
+   
