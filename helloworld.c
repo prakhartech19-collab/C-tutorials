4515a370f0f09db40025a509a11660507de328c7
prakhartech19-collab <prakhartech19@gmail.com>
2026-08-20 21:20:48 +0530

    intro of variable

--- helloworld.c
+++ helloworld.c
@@ -1,35 +1,18 @@
 #include<stdio.h>
- 
- int HCF( int a, int b);
- int a,b ;  // a is less than b
- int n =1;
+
 
 
 
 int main(){
-     
- printf(" enter  a:");
- scanf("%d",&a);
- printf(" enter b:");
- scanf("%d", &b);
-
-   int ans = HCF( a, b);
-   printf("%d", ans);
+ int a =5;
+ int b = 8;
+ 
+a = a + b;
+b = a - b;
+a = a - b;
+printf(" after swapping  a= %d and b = %d ",a,b);
   return 0;
   }
-  int HCF( int a , int b ){
-
-    int hcf=1;
-
-    for( int n = 1; n<=(a< b? a:b);n++){
-   
- 
-if( a% n ==0 && b% n == 0){
-        hcf = n;
-    }
-  } 
-  return hcf;
-}
 
   
  
