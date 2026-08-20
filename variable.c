83914c167b15e66bbd0e6b3080753ab6940ebc2c
prakhartech19-collab <prakhartech19@gmail.com>
2026-08-20 22:28:51 +0530

    sum of two numbers given by users

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+
+int main(){
+int a ,  b;
+
+printf("enter two numbers :a && b");
+scanf("%d%d", &a, &b);
+ 
+ printf("sum of given two numbers is : %d", a + b);
+  return 0;
+}
