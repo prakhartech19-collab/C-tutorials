4603e1e6b67cbd3674900d3bd45e62869063f2d1
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-01 07:59:22 +0530

    basic use of function

---
+++ function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void printHello();
+void printGoodbye();
+
+
+int main(){
+ printHello();
+ printGoodbye();
+}
+
+ void printHello(){
+    printf("hello\n");
+ }
+ void printGoodbye(){
+    printf("Goodbye\n");
+ }
