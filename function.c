d32bcc71750af1173f776e6909ac07023b3d6bbd
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-02 10:43:51 +0530

    function for calculation of squares

--- function.c
+++ function.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<math.h>
+
   void printPrice(float value);
-
+  
 int main(){
-  float value = 100.0;
-   printPrice(value);
+  // float value = 100.0;
+  //  printPrice(value);
+   
 
+   int n = 4;
+    printf("%f", pow(n,2));
  return 0;
 }
-  void printPrice(float value){
-    value = value +(0.18 * value);
-    printf(" price with gst is%f", value);
-  }
