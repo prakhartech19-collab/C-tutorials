3d8a01bcff5cd0ac8ec72fb335030e152ee626dc
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-02 20:11:37 +0530

    basic question on pointers

--- pointers.c
+++ pointers.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
 int main(){
- int age = 22;
- int *ptr = &age;
+ int x ;
+ int *ptr;
+  
+ ptr=&x;
+ *ptr = 0;// this is why x = 0
 
- //address 
- printf("%p\n", ptr);
- 
+ printf("x = %d\n",x); //  here x is 0 because we have assigned 0 to the address of x using pointer ptr
+ printf("*ptr = %d\n",*ptr);// here x is 0 because we have assigned 0 to the address of x using pointer ptr
+
+ *ptr +=5; // x  = 5
+ printf("x = %d\n",x); //prints 5
+ printf("*ptr = %d\n",*ptr);//prints 5
+
+ (*ptr)++;
+ printf("x = %d\n",x); // x =6
+ printf("*ptr = %d\n",*ptr);// x =6
+
  return 0;   
 }
