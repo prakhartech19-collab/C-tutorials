e25cd218615c8b9037e4a822e45bd1165b929fe1
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-02 12:03:55 +0530

    with the help of recursive function we are finding sum of n natural number

--- Recursion.c
+++ Recursion.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
- void printHW( int count);
+ int sum(int n);
+ 
+
 
 int main(){
-  printHW(5);
+ printf(" sum is %d", sum(5));
 return 0;
 }
 
 // recursive function
- void printHW( int count){
-    if(count == 0){
- return;
-    }  
-      printf("Hello world\n");
-     printHW(count - 1);
- }
+  int sum(int n){
+    if( n== 1){
+        return 1;
+    }
+    int sumNm1 = sum(n-1); // sum of 1 to n -1
+    int sumN = sumNm1 + n;
+    return sumN;
+  }
