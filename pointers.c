bed636a52bac3cb4eaeb743b4ca3d6825919e2e9
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-03 19:35:23 +0530

    useing pointers for  swaping of the value  and also the use of functions

--- pointers.c
+++ pointers.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
+void swap( int a , int b);
+void _swap( int *a, int *b);
+
 int main(){
- int x ;
- int *ptr;
-  
- ptr=&x;
- *ptr = 0;// this is why x = 0
-
- printf("x = %d\n",x); //  here x is 0 because we have assigned 0 to the address of x using pointer ptr
- printf("*ptr = %d\n",*ptr);// here x is 0 because we have assigned 0 to the address of x using pointer ptr
-
- *ptr +=5; // x  = 5
- printf("x = %d\n",x); //prints 5
- printf("*ptr = %d\n",*ptr);//prints 5
-
- (*ptr)++;
- printf("x = %d\n",x); // x =6
- printf("*ptr = %d\n",*ptr);// x =6
-
- return 0;   
+   int x = 3,  y= 5;
+  _swap(&x, &y);
+   printf("x = %d & y = %d\n", x , y );
+return 0;   
 }
+
+// call by refference
+ void _swap(int *a , int *b){
+    int t = *a;
+    *a = *b;
+    *b = t ;
+ }
+//  void swap( int a, int b){
+//     int t= a;
+//      a =b;
+//       b = t;
+//    printf("a = %d & b = %d\n", a,b );
+//  }
