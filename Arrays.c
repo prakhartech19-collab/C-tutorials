544c5363fa1a1c876de2cc6b540b1a38c4e956f8
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-05 15:11:22 +0530

    useing array as a pointer

--- Arrays.c
+++ Arrays.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
+ void printNumbers(int *arr,int n);
+
 int main(){
-   int aadhar[5];
-
-   //output
-   int *ptr = &aadhar[0];
-   for(int i=0; i<5 ; i++){
-    printf("%d index :", i);
-    scanf("%d", (ptr + i));
-   }
-   //output
-   for(int i=0 ; i<5; i++){
-    printf("%d index= %d\n", i ,aadhar[i]);
-
-   }
-   
-
+   int arr[] = {1,2,3,4,5,6};
+   printNumbers(arr, 6);
     return 0;
+}
+
+void printNumbers(int *arr, int n){
+  for(int i=0; i<n; i++){
+   printf("%d\t", arr[i]) ;
+  }
+  printf("\n");
 }
