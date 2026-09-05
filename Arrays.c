ad15b9f6cf5572266fccf103dddfb39392d63dc5
prakhartech19-collab <prakhartech19@gmail.com>
2026-09-05 11:54:55 +0530

    here i am trying to say that in an array if we use pointer arithmatics then the address will change by the datatype units

--- Arrays.c
+++ Arrays.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-// with the help of arrays we are entering and printing the prices of 3 items
+
 int main(){
-  
-  float price[3];
- printf(" enter P1 :");
- scanf("%f", &price[0]);
-
- printf("enter P2:");
- scanf("%f", &price[1]);
-
- printf("enter P3 :");
- scanf("%f", &price[2]);
- 
-printf("Total  P1 is :%f",price[0] + (0.18*price[0]));
-printf("Total  P2 is :%f",price[1] + (0.18*price[1]));
-printf("Total  P3 is :%f",price[2] + (0.18*price[2]));
-
+  int age = 22;
+  int *ptr = &age;
+  printf("ptr = %u", ptr);
+  ptr++;
+printf("ptr = %u", ptr);
     return 0;
 }
